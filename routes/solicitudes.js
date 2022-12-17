259dae81950a7e618ec01179eadd16451be0a713
Paul <paulqueza123@gmail.com>
2022-12-17 02:25:36 -0300

    WTF

--- routes/solicitudes.js
+++ routes/solicitudes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // importar el modelo nota
-const solicitudes = requiere ('../models/solicitudes');
+const solicitudes = require ('../models/solicitudes');
 
 // Agregar un nuevo
 router.post('/nuevo_solicitud', async(req, res) => {
